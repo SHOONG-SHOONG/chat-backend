4b35af6d938487b0e8c6f4b9ae406d7f5dd0db60
wns5120 <ghwns7171@naver.com>
2025-05-23 20:21:19 +0900

    feat: cors 설정

--- chat-backend/src/main/java/com/example/chatbackend/config/CorsConfig.java
+++ chat-backend/src/main/java/com/example/chatbackend/config/CorsConfig.java
@@ -3,27 +3,32 @@ package com.example.chatbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.List;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("https://shoong.store", "https://*.shoong.store"
-                ,"http://localhost:3000"
-                , "http://192.168.0.6"));
-
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
-        config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
-        config.setAllowCredentials(true); // 인증 정보(쿠키, 헤더 등) 허용
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:3000",
+                "http://192.168.0.6",
+                "https://shoong.store",
+                "https://*.shoong.store"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return source;
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0); // 가장 먼저 실행되도록 설정
+        return bean;
     }
 }
